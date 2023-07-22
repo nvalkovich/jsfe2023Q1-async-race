98e97e8770c2c4749472c317c83d4c6ff8d30c67
nvalkovich <anastasiya.valkovich@mail.ru>
2023-07-22 19:53:16 +0300

    feat: implement routing, implement render winers page

--- async-race/src/modules/app.ts
+++ async-race/src/modules/app.ts
@@ -1,25 +1,56 @@
 import GaragePage from './pages/garage/garagePage';
+import WinnersPage from './pages/winners/winnersPage';
 import Page from './pages/page';
+import PageIds from '../types/enums';
 import HeaderButtons from './headerBtns';
 
 class App {
   private static container: HTMLElement = document.body;
 
+  private static defaultPageID = 'garage';
+
+  private initialPage: GaragePage;
+
   private header: HeaderButtons;
 
   constructor() {
+    this.initialPage = new GaragePage('garage');
     this.header = new HeaderButtons('header', 'header');
   }
 
   public static renderNewPage(id: string):void {
-    const page: Page = new GaragePage(id);
-    const pageHTML = page.render();
-    this.container.append(pageHTML);
+    const currentPageHTML = document.querySelector(`#${this.defaultPageID}`);
+    if (currentPageHTML) {
+      currentPageHTML.remove();
+    }
+    let page: Page | null = null;
+    if (id === PageIds.Garage) {
+      page = new GaragePage(id);
+    } else if (id === PageIds.Winners) {
+      page = new WinnersPage(id);
+    }
+    if (page) {
+      const pageHTML = page.render();
+      pageHTML.id = App.defaultPageID;
+
+      this.container.append(pageHTML);
+    }
   }
 
   public run(): void {
+    this.route();
     App.container.append(this.header.render());
+    const mainPageHtml = this.initialPage.render();
+    App.container.append(mainPageHtml);
     App.renderNewPage('garage');
+    window.location.hash = `#${App.defaultPageID}`;
+  }
+
+  private route():void {
+    window.addEventListener('hashchange', (e) => {
+      const hash = window.location.hash.slice(1);
+      App.renderNewPage(hash);
+    });
   }
 }
 
